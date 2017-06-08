dc638bf89dffc70adb815877fd55bb9a4a14213e
Kos Klimov <klimnet77@gmail.com>
2017-06-08 12:16:52 +0300

    Create LastElementInsertionSorter.java
    last elem check added

--- src/main/java/com/revotech/sorter/LastElementInsertionSorter.java
+++ src/main/java/com/revotech/sorter/LastElementInsertionSorter.java
@@ -2,8 +2,8 @@ package com.revotech.sorter;
 
 public class LastElementInsertionSorter {
     public int[] sort(int[] arr) {
-        boolean inserted = false;
         int elem = arr[arr.length - 1];
+        boolean inserted = elem < arr[arr.length - 2];
         for (int i = arr.length - 1; i > 0; i--) {
             if (!inserted) {
                 if (arr[i - 1] > elem) {
